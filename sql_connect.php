7b1e18f22ce2c9fe78f25a35f7187658120c3db3
Surya Teja Achanta <suryateja.a16@iiits.in>
2018-07-05 16:41:27 +0530

    Update sql_connect.php

--- sql_connect.php
+++ sql_connect.php
@@ -1,7 +1,7 @@
 <?php
     $mysql_host = '127.0.0.1';
     $mysql_user = 'root';
-    $mysql_pass = '---your database password---';
+    $mysql_pass = '---your password---';
     $conn = mysqli_connect($mysql_host,$mysql_user,$mysql_pass);
     if($conn && mysqli_select_db($conn,'a_database')){
         echo 'connection established successfully';
