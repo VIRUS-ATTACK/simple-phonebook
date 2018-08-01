e642de1838cc602eb3ee4bbd5d3c67765452eb7a
Surya Teja Achanta <suryateja.a16@iiits.in>
2018-08-01 10:04:34 +0530

    Update sql_connect.php

--- sql_connect.php
+++ sql_connect.php
@@ -4,7 +4,8 @@
     $mysql_pass = '---your password---';
     $conn = mysqli_connect($mysql_host,$mysql_user,$mysql_pass);
     if($conn && mysqli_select_db($conn,'phonebook1')){
-        echo 'connection established successfully';
+        //echo 'connection established successfully';
+        ;
     }
     else{
         die('connection failed');
